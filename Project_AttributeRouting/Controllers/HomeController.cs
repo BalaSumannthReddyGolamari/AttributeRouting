56ce44e6ba20700490326f4d2a8a014f4a007b45
BalaSumannthReddyGolamari <34880804+BalaSumannthReddyGolamari@users.noreply.github.com>
2018-01-02 17:26:26 +0530

    Add project files.

---
+++ Project_AttributeRouting/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_AttributeRouting.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Route("Group5/{id}")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project_AttributeRouting/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_AttributeRouting.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int Age { get; set; }
+    }
+}
